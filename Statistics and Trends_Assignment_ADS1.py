4acd7a8dc4c3e17ad9cc2c8bd44c33629424bd0b
ogespride <100952905+ogespride@users.noreply.github.com>
2022-12-11 01:07:09 +0000

    Add files via upload
    Version 3 =Upload showing heatmap

--- Statistics and Trends_Assignment_ADS1.py
+++ Statistics and Trends_Assignment_ADS1.py
@@ -94,3 +94,92 @@ plt.ylabel('Arable land')
 plt.legend(bbox_to_anchor=(1,1), loc="upper left")
 #plt.rcParams["figure.dpi"] = 1000
 plt.show() 
+
+#Statistical properties of indicator
+#Urban population
+print(indicator_urb_pop.describe())
+#CO2 Emission from liquid fuel consumption
+print(indicator_co2.describe())
+#Electric Power Consumption
+print(indicator_elec.describe())
+#Arable land(% of land Area)
+print(indicator_arland.describe())
+
+#Showing Correlations between Countries and Indicators
+#Argentina
+Argentina = pd.DataFrame(
+{'Urban Population': year_urb_pop['Argentina'],
+'Co2 emission': year_co2['Argentina'],
+'Elec. Access': year_elec['Argentina']},
+['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010'])
+print(Argentina.corr())
+
+#Using heatmap graph to show correlation between the indicators and Argentina
+plt.figure(figsize=(8,5))
+sns.heatmap(Argentina.corr(),annot=True,cmap='Greens')
+plt.title('Correlation heatmap Argentina')
+
+#Showing Correlations between Countries and Indicators
+#China
+China = pd.DataFrame(
+{'Urban Population': year_urb_pop['China'],
+'Co2 emission': year_co2['China'],
+'Elec. Access': year_elec['China']},
+['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010'])
+print(China.corr())
+
+#Using heatmap graph to show correlation between the indicators and China
+plt.figure(figsize=(8,5))
+sns.heatmap(China.corr(),annot=True,cmap='Greens')
+plt.title('Correlation heatmap China')
+
+plt.show()
+
+#Showing Correlations between Countries and Indicators
+#Nigeria
+Nigeria = pd.DataFrame(
+{'Urban Population': year_urb_pop['Nigeria'],
+'Co2 emission': year_co2['Nigeria'],
+'Elec. Access': year_elec['Nigeria']},
+['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010'])
+print(Nigeria.corr())
+
+#Using heatmap graph to show correlation between the indicators and Nigeria
+plt.figure(figsize=(8,5))
+sns.heatmap(Nigeria.corr(),annot=True,cmap='Greens')
+plt.title('Correlation heatmap Nigeria')
+
+plt.show()
+
+#Showing Correlations between Countries and Indicators
+#United Arab Emirates
+UnitedArabEmirates = pd.DataFrame(
+{'Urban Population': year_urb_pop['United Arab Emirates'],
+'Co2 emission': year_co2['United Arab Emirates'],
+'Elec. Access': year_elec['United Arab Emirates']},
+['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010'])
+print(UnitedArabEmirates.corr())
+
+#Using heatmap graph to show correlation between the indicators and United Arab Emirates
+plt.figure(figsize=(8,5))
+sns.heatmap(UnitedArabEmirates.corr(),annot=True,cmap='Greens')
+plt.title('Correlation heatmap UnitedArabEmirates')
+
+plt.show()
+
+#Showing Correlations between Countries and Indicators
+#United States
+UnitedStates = pd.DataFrame(
+{'Urban Population': year_urb_pop['United States'],
+'Co2 emission': year_co2['United States'],
+'Elec. Access': year_elec['United States']},
+['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010'])
+print(UnitedStates.corr())
+
+#Using heatmap graph to show correlation between the indicators and United States
+plt.figure(figsize=(8,5))
+sns.heatmap(UnitedStates.corr(),annot=True,cmap='Greens')
+plt.title('Correlation heatmap UnitedStates')
+
+plt.show()
+
